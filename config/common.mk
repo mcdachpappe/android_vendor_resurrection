be8a09828fb6355ed9dad8013b737930e6d8a56b
danny <noreference@web.de>
2018-05-28 16:45:56 +0200

    vendor_rr_prebuild: add some google apps
    * Google camera mod:
    by @SKULSHADY
    thanks for his gcam mod 5.1.018_v5.1
    
    * Google MarkUp backport:
    by @mg712702
    from: https://github.com/BootleggersROM/vendor_bootleggers/commit/3fac42f4b7a564a24b78bb328b94fc78016d3389
    
    * Google exclusive features
    by @eldainosor
    from: https://github.com/BootleggersROM/vendor_bootleggers/commit/58c16ffc1a7673d7d6b6373fe45bb471714fc9db
    
    Change-Id: I2d3306211a248017e26f2106b3fd37041b905f5d

--- config/common.mk
+++ config/common.mk
@@ -157,6 +157,8 @@ PRODUCT_PACKAGES += \
     LineageSettingsProvider \
     Eleven \
     ExactCalculator \
+    GCam \
+    GoogleMarkup \
     Launcher3 \
     LockClock \
     Updater \
@@ -180,7 +182,14 @@ PRODUCT_COPY_FILES += \
     vendor/rr/prebuilt/fonts/roadrage/road_rage.ttf:system/fonts/RoadRage-Regular.ttf \
     vendor/rr/prebuilt/fonts/snowstorm/snowstorm.ttf:system/fonts/Snowstorm-Regular.ttf \
     vendor/rr/prebuilt/fonts/vcrosd/vcr_osd_mono.ttf:system/fonts/ThemeableFont-Regular.ttf \
-    vendor/rr/prebuilt/fonts/googlesans/GoogleSans-Medium.ttf:system/fonts/GoogleSans-Medium.ttf
+    vendor/rr/prebuilt/fonts/googlesans/GoogleSans-Regular.ttf:system/fonts/GoogleSans-Medium.ttf \
+
+# Google Exclusives
+PRODUCT_COPY_FILES += \
+    vendor/rr/config/permissions/google_build.xml:system/etc/sysconfig/google_build.xml \
+    vendor/rr/config/permissions/nexus.xml:system/etc/sysconfig/nexus.xml \
+    vendor/rr/config/permissions/pixel_2017.xml:system/etc/sysconfig/pixel_2017.xml \
+    vendor/rr/config/permissions/pixel_2017_exclusive.xml:system/etc/sysconfig/pixel_2017_exclusive.xml
 
 # Berry styles
 PRODUCT_PACKAGES += \
